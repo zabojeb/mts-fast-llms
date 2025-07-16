854fb4e8eb2fbbfb6aeefc0e89f5de0bbe7f865a
mixadyt-star <riseofkingdoms10kkmoschi@gmail.com>
2025-07-16 14:50:49 +0300

    adsgrgvjkhgkusfkruiv

--- quantization/methods/__init__.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-# Module done on 16.07 in 11:28
-
-from .affine_transform import affine_transform
-from .to_dtype import weights_to_dtype
-=======
-from .to_dtype import weights_to_dtype
-from .scaled import scaled_weights
->>>>>>> a523bfc160e57732f7fa65d0fad6e9cf377873d2
--- quantization/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-# Module done on 16.07 in 11:31
-
-from bitsandbytes.nn import Linear4bit
-
-from .qlinear import LinearAffine8bit
-=======
-from .qlinear import QuantizedLinear
->>>>>>> a523bfc160e57732f7fa65d0fad6e9cf377873d2
