c86b2254f496efd73bb5f055ca47b668be116aa9
Denbug <dipetrin12@yandex.ru>
2025-07-19 09:03:54 +0300

    optimized realisation of metrics beta with changed config for q

--- quantization/config.py
+++ quantization/config.py
@@ -1,6 +1,6 @@
-# Module done on 17.07 in 14:33
+# Module done on 19.07 in 9:03
 
-from typing import Optional, Dict, List
+from typing import Optional, Dict, List, Type
 from torch import dtype
 from torch import nn
 
@@ -10,13 +10,19 @@ QINT4 = 4
 
 class QuantizationConfigLayer:
     def __init__(
-        self,
-        qtype: int,
-        fraction: Optional[float] = 1,
-        names: Optional[List[str]] = None,
+            self,
+            qtype: int,
+            fraction: Optional[float] = 1,
+            names: Optional[List[str]] = None,
     ):
-        # TODO: description
+        """
+        Configuration for quantizing a layer
 
+        Args:
+            qtype: Quantization type (QINT8 or QINT4)
+            fraction: Fraction of parameters to quantize (0 to 1)
+            names: List of parameter names to quantize (if specified, fraction is ignored)
+        """
         if names is not None:
             fraction = None
 
@@ -32,19 +38,19 @@ class QuantizationConfigLayer:
 
 class QuantizationConfig:
     def __init__(
-        self,
-        compute_dtype: Optional[dtype] = None,
-        layers: Dict[nn.Module, QuantizationConfigLayer] = None,
+            self,
+            compute_dtype: Optional[dtype] = None,
+            layers: Optional[Dict[Type[nn.Module], QuantizationConfigLayer]] = None,
     ):
         """
-        Kwargs:
-            compute_dtype: dtype for weight
+        Quantization configuration for a model
+
+        Args:
+            compute_dtype: Data type for computations
+            layers: Dictionary mapping layer types to their quantization configs
         """
-
-        # TODO: description
-
         self.compute_dtype = compute_dtype
-        self.layers = layers
+        self.layers = layers or {}
 
     def __repr__(self):
         return "QuantizationConfig(compute_dtype={}, layers={})".format(
